8b043523546514403018c49b7c257da77ab99042
Godswill Chidera <bobbylivinus@gmail.com>
2023-04-25 18:24:13 +0100

    updated home background image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import homeImage from "../images/vanlifeHome.jpg"
 
 function Home() {
   return (
-    <div className='home'>
+    <div className='home' style={{backgroundImage: `url(${homeImage})`}}>
         <h1>You got the travel plans, we got the travel vans.</h1>
         <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
         <Link to="/vans">Find your van</Link>
